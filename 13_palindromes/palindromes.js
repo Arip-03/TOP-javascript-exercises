5ccc6131a8868ffd0075d01edbe32295f45a5906
Arip Srpd <kyriezoldyck03@gmail.com>
2025-09-10 21:09:25 +0800

    Arip test 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let processed = str.toLowerCase();
 
+  processed = processed.replace(/[^a-z0-9]/g, "");
+
+  const reversed = processed.split("").reverse().join("");
+  return processed === reversed;
 };
 
 // Do not edit below this line
